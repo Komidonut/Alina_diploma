8772022067d650105275b20f73c5115b25466203
djon smith <djonsmith@pop-os.localdomain>
2020-06-11 12:18:55 +0300

    Added header

--- map.js
+++ map.js
@@ -27,7 +27,7 @@ marker1.on("click", function () {
       document.getElementById("markerHeader").innerHTML = "Название Маркера 1";
       document.getElementById("markerDescription").innerHTML =
         "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?";
-      document  ``.getElementById("markerImage").src = "img/marker1.jpg";
+      document.getElementById("markerImage").src = "img/marker1.jpg";
     }, 200);
     clicked = this.getLatLng();
   }
